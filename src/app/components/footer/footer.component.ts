54a6b8a95efe43b2dd2142c29de75afccf889174
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2018-03-02 21:52:01 +0100

    Added about anilist section to the footer component.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
   gitHubProjectUrl: string = 'https://github.com/jesuscc1993/angular-material-demo';
 
-  constructor() {
-
-  }
-
-  ngOnInit(): void {
-
+  constructor(
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
+    iconRegistry.addSvgIcon('anilist', sanitizer.bypassSecurityTrustResourceUrl('assets/pictures/vectorial/icons/anilist.svg'));
   }
 
 }
