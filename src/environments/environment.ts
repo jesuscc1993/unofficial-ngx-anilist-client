6f1eb92a76c04433edf6b6886f23d17b735c031a
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2018-03-02 19:08:33 +0100

    Improvements on the search form.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment: any = {
-  anilist_client_id: 284,
+  anilist_client_id: -1,
   production: false
 };
