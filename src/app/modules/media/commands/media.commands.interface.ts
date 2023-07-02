7bab3f6d17618cb49dbf5ecde273f8f4e7edac1e
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2023-07-02 21:02:27 +0200

    Improved typing.

--- src/app/modules/media/commands/media.commands.interface.ts
+++ src/app/modules/media/commands/media.commands.interface.ts
@@ -1,32 +1,54 @@
 import { Observable } from 'rxjs';
 
 import {
+  ListEntriesByStatus,
   ListEntry,
   ListEntryStatus,
 } from '../../shared/types/anilist/listEntry.types';
 import { Media } from '../../shared/types/anilist/media.types';
 import { PageInfo, PageQuery } from '../../shared/types/anilist/pageInfo.types';
 import { User } from '../../shared/types/anilist/user.types';
+import { MediaExportEntry, MediaPage } from '../../shared/types/media.types';
 import { SearchFilters } from '../api/media.types';
 
 export interface MediaCommandsInterface {
-  deleteListEntry(listEntry: ListEntry);
-  getFavouriteIDs();
+  deleteListEntry(listEntry: ListEntry): Observable<unknown>;
+
+  getFavouriteIDs(): Observable<number[]>;
+
   getListEntries(): Observable<ListEntry[]>;
-  getListEntriesExport();
-  getListEntriesGroupedByStatus();
+
+  getListEntriesExport(): Observable<MediaExportEntry[]>;
+
+  getListEntriesGroupedByStatus(): Observable<ListEntriesByStatus>;
+
   getMediaFromIds(
     mediaIds: number[],
     query: SearchFilters,
     pageQuery?: PageQuery
   ): Observable<{ media: Media[]; pageInfo: PageInfo }>;
+
   getPendingMedia(): Observable<ListEntry[]>;
-  queryFavouriteIDs(user: User);
-  queryGenres();
-  queryListEntries();
-  queryMedia(query: SearchFilters, pageQuery?: PageQuery);
+
+  queryFavouriteIDs(user: User): void;
+
+  queryGenres(): Observable<string[]>;
+
+  queryListEntries(): Observable<ListEntry[]>;
+
+  queryMedia(
+    query: SearchFilters,
+    pageQuery?: PageQuery
+  ): Observable<MediaPage>;
+
   queryRelatedMediaIds(): Observable<number[]>;
-  saveListEntry(listEntry: ListEntry);
-  saveMediaWithStatus(media: Media, status: ListEntryStatus);
-  toggleFavourite(user: User, media: Media);
+
+  saveListEntry(listEntry: ListEntry): Observable<ListEntry>;
+
+  saveMediaWithStatus(
+    media: Media,
+    status: ListEntryStatus
+  ): Observable<ListEntry>;
+
+  toggleFavourite(user: User, media: Media): Observable<number>;
 }
--- src/app/modules/shared/types/media.types.ts
+++ src/app/modules/shared/types/media.types.ts
@@ -1,7 +1,18 @@
+import { ListEntryStatus } from './anilist/listEntry.types';
 import { Media } from './anilist/media.types';
 import { PageInfo } from './anilist/pageInfo.types';
 
 export type MediaPage = {
   media: Media[];
   pageInfo: PageInfo;
+};
+
+export type MediaExportEntry = {
+  entryId: number;
+  mediaId: number;
+  progress: number;
+  repeat: number;
+  scoreRaw: number;
+  status: ListEntryStatus;
+  title: string;
 };
