4a6440ab042c294a5da52f43b26d5b04e2608ec6
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2023-10-08 14:01:54 +0200

    Added more score options.

--- src/app/modules/shared/constants/media.constants.ts
+++ src/app/modules/shared/constants/media.constants.ts
@@ -31,4 +31,4 @@ export const mediaCountries = [
   MediaCountry.TW,
 ];
 
-export const mediaScores = [undefined, 5, 6, 7, 8, 9];
+export const mediaScores = [undefined, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5];
