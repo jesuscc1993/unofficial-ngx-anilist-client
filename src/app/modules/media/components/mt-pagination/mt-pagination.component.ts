f5c2f7d380ea649583222ade8bb7f6d26e07e553
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2023-12-31 11:07:27 +0100

    Fixed missing page size options.

--- src/app/modules/media/components/mt-pagination/mt-pagination.component.ts
+++ src/app/modules/media/components/mt-pagination/mt-pagination.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 
+import { pageSizeOptions } from '../../../../app.constants';
 import { PageInfo } from '../../../shared/types/anilist/pageInfo.types';
 
 @Component({
@@ -12,6 +13,8 @@ export class MtPaginationComponent {
   @Input() class?: string;
   @Input() pagination?: PageInfo;
   @Output() changePage: EventEmitter<PageEvent>;
+
+  pageSizeOptions = pageSizeOptions;
 
   constructor() {
     this.changePage = new EventEmitter<PageEvent>();
