4f42d3b5c18c903411b203f260bfde0a3dd3a4f5
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2026-09-26 01:11:40 +0200

    Updated display of score and progress

--- src/app/modules/media/components/mt-media-score/mt-media-score.component.ts
+++ src/app/modules/media/components/mt-media-score/mt-media-score.component.ts
@@ -1,16 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 import { ListEntry } from '../../../shared/types/anilist/listEntry.types';
 import { Media } from '../../../shared/types/anilist/media.types';
+import { getScoreColor, getScoreTooltip } from '../../domain/media.domain';
 
 @Component({
   selector: 'mt-media-score',
   templateUrl: './mt-media-score.component.html',
   styleUrls: ['./mt-media-score.component.scss'],
+  host: {
+    '[class.float]': 'shouldFloat',
+  },
   standalone: false,
 })
 export class MtMediaScoreComponent {
+  private translate = inject(TranslateService);
+
+  @Input() isPill = false;
   @Input() listEntry?: ListEntry;
   @Input() media!: Media;
-  @Input() columnMode?: boolean;
+  @Input() shouldFloat = false;
+
+  readonly getScoreColor = getScoreColor;
+
+  getScore() {
+    return this.listEntry?.scoreRaw || this.media.averageScore;
+  }
+
+  getTooltip() {
+    return getScoreTooltip(this.translate, this.media, this.listEntry);
+  }
 }
--- src/app/modules/media/domain/media.domain.ts
+++ src/app/modules/media/domain/media.domain.ts
@@ -1,5 +1,6 @@
 import { ElementRef } from '@angular/core';
 import { Sort } from '@angular/material/sort';
+import { TranslateService } from '@ngx-translate/core';
 
 import { animeFormats } from '../../anime/constants/anime.constants';
 import { mangaFormats } from '../../manga/constants/manga.constants';
@@ -56,6 +57,36 @@ export const getFormattedMediaYearRange = ({ startDate, endDate }: Media) => {
 
 export const getSanitizedMediaDescription = (media: Media) => {
   return media.description.replace(/h\d+>/g, 'strong>');
+};
+
+export const getScoreColor = (score: number) => {
+  if (score >= 70) return 'green';
+  if (score >= 50) return 'orange';
+  return 'red';
+};
+
+export const getScoreTooltip = (
+  translate: TranslateService,
+  media: Media,
+  listEntry?: ListEntry
+) => {
+  const tooltipLines: string[] = [];
+  if (listEntry?.scoreRaw) {
+    tooltipLines.push(
+      `${translate.instant('media.userScore')}: ${listEntry.scoreRaw}`
+    );
+  }
+  if (media.averageScore) {
+    tooltipLines.push(
+      `${translate.instant('media.averageScore')}: ${media.averageScore}`
+    );
+  }
+  if (media.meanScore) {
+    tooltipLines.push(
+      `${translate.instant('media.meanScore')}: ${media.meanScore}`
+    );
+  }
+  return tooltipLines.join('\n');
 };
 
 export const pad = (value: number | string, length = 2, character = '0') => {
